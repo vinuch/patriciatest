f01843f2c0c69cfaa60f5eb6017d218a6a9dd124
vinuch <vincentedeh42@gmail.com>
2021-09-10 18:12:45 +0100

    bug fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     <div className=" flex justify-between fixed w-full md:w-10/12 z-20 bg-white">
       <div className="bg-white md:w-8/12 px-6 py-9 flex items-center">
         <img src={leftArrow} alt="back icon" /> 
-        <h3 className="text-3xl ml-2">Cards</h3>
+        <h3 className="text-3xl ml-2 text-gray5 font-medium">Cards</h3>
       </div>
       <div className="md:bg-gray4 md:w-4/12 flex items-center justify-end md:px-6">
         <button className="bg-white p-2 mx-2 rounded-md">
